eaa4b3e7bf91506d75a878918122f6fd204d7fdc
Belov <Vladbel1989@gmail.com>
2021-07-24 01:55:38 +0700

    git-rebase 2
    Merge branch 'get-merge'

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -4,9 +4,10 @@
 count=1
 for param in "$@"; do
 <<<<<<< HEAD
-    echo "\$@ Parameter #$count = $param"
+    echo "Next parameter: $param"
     count=$(( $count + 1 ))
 done
 
 echo "====="
 
+>>>>>>> 7b17db6 (git-rebase 2)
